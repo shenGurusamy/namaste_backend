3e35161030292ec6cc5dca964029fdb8f640634f
shenGurusamy <rjpmpandian@gmail.com>
2026-06-20 08:25:15 +0530

    added err routes

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use (  (   req, res, next) => {
 app.use ( ( err, req, res, next) => {
     const status  = err.statusCode  || 500 
     const mess = err.message || " something went wrong"
-    res.status( status).send( mess )
+    res.status( status).send(mess)
 })
 
  
