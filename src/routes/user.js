c9e3514e429955e4aa5f47edd1a57f1d6da0b55b
shenGurusamy <rjpmpandian@gmail.com>
2026-06-21 23:12:23 +0530

    Implemented all connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,34 @@ const userRouter = express.Router()
 const { userAuth } = require("./../middlewares/auth")
 const Connection = require("./../models/connectionRequestSchema")
 
-userRouter.get( "/connections", userAuth , async (req, res) =>{
+const USER_SAFE_DATA ="firstName lastName gender about photoUrl"
 
+userRouter.get( "/user/connections", userAuth , async (req, res) =>{
+    try{
+        const loggedinUser = req.user
+        const connectionList = await Connection.find( {
+            $or:[
+                {
+                    toUserId: loggedinUser._id ,status :"accepted"
+                },
+                {
+                    fromUserId: loggedinUser._id ,status :"accepted"
+                }
+            ]
+            
+        }).populate( "fromUserId"  , USER_SAFE_DATA)
+        const data = connectionList.map( row => row.fromUserId)
+        if ( data.length) {
+            res.send(data)
+        }
+        else{
+            res.send(`No pending request`)
+        }
+
+    }
+    catch(err){
+        res.status(400).send(err.message)
+    }
 })
 userRouter.get( "/user/request/received", userAuth , async (req, res) =>{
     //get all the pedning connection Request for the logged in User
@@ -13,10 +39,11 @@ userRouter.get( "/user/request/received", userAuth , async (req, res) =>{
         const connectionRequest = await Connection.find( { 
             toUserId : loggedUser._id,
             status : "interested"
-        } ).populate( "fromUserId" , ["firstName" , "lastName", "about" , "gender" ,"photoUrl"])
+        } ).populate( "fromUserId" , USER_SAFE_DATA)
         
-        if ( connectionRequest.length) {
-            res.send(connectionRequest)
+        const data = connectionRequest.map ( row => row.fromUserId)
+        if ( data.length) {
+            res.send(data)
         }
         else{
             res.send(`No pending request`)
