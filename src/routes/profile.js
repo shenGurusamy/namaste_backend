411b3a7de0e0a466b4799288e64d9db756291c2c
shenGurusamy <rjpmpandian@gmail.com>
2026-06-21 14:00:14 +0530

    profile edit API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,14 +3,35 @@ const { userAuth } = require("./../middlewares/auth")
 
 
 const User = require("./../models/userSchema")
-const validations =  require("./../utils/validation")
+const {validations , userValidation} =  require("./../utils/validation")
 const bcrypt = require("bcrypt")
 
 const profileRouter = express.Router()
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     res.send(req.user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    console.log("first")
+    userValidation(req.body)
+    console.log("after validation")
+ 
+    const loggedinUser  = req.user
+    Object.keys(req.body).forEach( k => loggedinUser[k] = req.body[k])
+    const user = await loggedinUser.save()
+    if ( user ){
+      res.send(user)
+    }
+    else {
+      throw new Error("")
+    }
+    
   } catch (err) {
     res.status(400).send("Something went wrong");
   }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,16 +1,29 @@
 const validator   = require("validator")
+const { all } = require("../routes/auth")
 const validations = ( req ) =>{
     const { firstName, lastName, email } = req.body
 
-    // if ( !firstName || !lastName) {
-    //     throw new Error( "Name is missing")
-    // }
-    // else if ( !validator.isEmail( email)){
-    //     throw new Error( "Please check your EMail ")
-    // }
+    if ( !firstName || !lastName) {
+        throw new Error( "Name is missing")
+    }
+    else if ( !validator.isEmail( email)){
+        throw new Error( "Please check your EMail ")
+    }
 
 
 }
 
-module.exports = { validations }
+const userValidation = ( req ) => {
+
+    const allowedFields = ["firstName" , "lastName", "about", "age", "skills" , "gender"]
+    console.log(req)
+
+    const isallowedfields = Object.keys(req).every( key => allowedFields.includes(key))
+    console.log(isallowedfields)
+    if ( !isallowedfields) {
+        throw new Error( "check all the fields")
+    }
+}
+
+module.exports = { validations, userValidation }
 
